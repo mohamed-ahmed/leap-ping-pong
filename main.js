e1eeecabde84ab57dafa91493579740af33a03c5
Mohamed Ahmed <mohamed.ahmed@live.ca>
2014-02-19 05:14:26 -0500

    trying to display high score in cookies

--- main.js
+++ main.js
@@ -386,6 +386,9 @@ function gameOver() {
   
   // Show the restart button
   restartBtn.draw();
+  if( points > parseInt(readCookie("high-score")) ){
+    createCookie("high-score", points )
+  }
 }
 
 // Function for running the whole animation
@@ -499,4 +502,45 @@ function movePaddle(y){
     //console.log("p.y: ");
     //console.log(p.y)
   }
+}
+
+  /*
+  creates cookies
+  @param {String} name -  of coookie
+  @param {String} valie - value of coookie
+  @param {number} days -  number of days for coookie to stay active
+  */
+function createCookie(name,value,days) {
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime()+(days*24*60*60*1000));
+    var expires = "; expires="+date.toGMTString();
+  }
+  else var expires = "";
+  document.cookie = name+"="+value+expires+"; path=/";
+}
+
+  /*
+  creates cookies
+  @param {String} name -  of coookie
+  @return {String} valie - value of coookie
+  */
+
+function readCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
+  /*
+  * erases cookies
+  * @param {String} name -  of coookie
+  */
+function eraseCookie(name) {
+  createCookie(name,"",-1);
 }
